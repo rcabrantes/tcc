3517eba01e4a6b245d9e074ac79d8e8b66ba9d7a
Renato Sinohara <renato@userlane.com>
2017-05-25 22:42:14 +0200

    Blinking app

---
+++ TCC/TCC/MainPage.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Devices.Gpio;
+
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace TCC
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private const int LED_PIN = 5;
+        private GpioPin pin;
+        private GpioPinValue pinValue;
+        private DispatcherTimer timer;
+        private SolidColorBrush redBrush = new SolidColorBrush(Windows.UI.Colors.Red);
+        private SolidColorBrush grayBrush = new SolidColorBrush(Windows.UI.Colors.LightGray);
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(500);
+            timer.Tick += Timer_Tick;
+            InitGPIO();
+            if (pin != null)
+            {
+                timer.Start();
+            }
+        }
+
+
+        private void Timer_Tick(object sender, object e)
+        {
+            if (pinValue == GpioPinValue.High)
+            {
+                pinValue = GpioPinValue.Low;
+                pin.Write(pinValue);
+                LED.Fill = redBrush;
+            }
+            else
+            {
+                pinValue = GpioPinValue.High;
+                pin.Write(pinValue);
+                LED.Fill = grayBrush;
+            }
+        }
+
+
+        private void InitGPIO()
+    {
+        var gpio = GpioController.GetDefault();
+            
+
+        // Show an error if there is no GPIO controller
+        if (gpio == null)
+        {
+            pin = null;
+            GpioStatus.Text = "There is no GPIO controller on this device.";
+            return;
+        }
+
+        pin = gpio.OpenPin(LED_PIN);
+        pinValue = GpioPinValue.High;
+        pin.Write(pinValue);
+        pin.SetDriveMode(GpioPinDriveMode.Output);
+
+        GpioStatus.Text = "GPIO pin initialized correctly.";
+
+    }
+}
+}
